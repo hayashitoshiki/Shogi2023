a0e847364c63918897dde9ca29e24cee5c171643
hayashitoshiki <tosikihayasi@outlook.jp>
2023-12-31 14:08:37 +0900

    createa ReStartButton

---
+++ presentation/home/src/main/java/com/example/home/game/compoment/button/ReStartButton.kt
@@ -0,0 +1,73 @@
+package com.example.home.game.compoment.button
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.home.R
+
+@Composable
+fun ReStartButton(modifier: Modifier = Modifier, onClick: () -> Unit) {
+    val showDialog = remember { mutableStateOf(false) }
+
+    IconButton(
+        modifier = modifier.size(48.dp),
+        onClick = { showDialog.value = true }
+    ) {
+        Icon(
+            modifier = modifier.size(32.dp),
+            painter = painterResource(R.drawable.ic_restart),
+            contentDescription = ""
+        )
+    }
+    if (showDialog.value) {
+        ConfirmReStartDialog(openDialog = showDialog, onClick = onClick)
+    }
+}
+
+@Composable
+fun ConfirmReStartDialog(
+    openDialog: MutableState<Boolean>,
+    onClick: () -> Unit
+) {
+    val context = LocalContext.current
+    AlertDialog(
+        onDismissRequest = {
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    openDialog.value = false
+                    onClick()
+                }
+            ) {
+                Text(context.getString(R.string.dialog_text_yes))
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    openDialog.value = false
+                }
+            ) {
+                Text(context.getString(R.string.dialog_text_no))
+            }
+        },
+        title = {
+            Text(context.getString(R.string.dialog_confirm_restart_title))
+        },
+        text = {
+            Text(context.getString(R.string.dialog_confirm_restart_body))
+        },
+    )
+}
