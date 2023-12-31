1574235d47bd597197214ec64e08a625aafcc17a
hayashitoshiki <tosikihayasi@outlook.jp>
2023-12-31 16:05:59 +0900

    add notBackKey logic

--- presentation/home/src/main/java/com/example/home/game/GameScreen.kt
+++ presentation/home/src/main/java/com/example/home/game/GameScreen.kt
@@ -1,13 +1,17 @@
 package com.example.home.game
 
+import android.util.Log
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -45,6 +49,9 @@ fun GameScreen(
             onClick = { navController.popBackStack() },
         )
     }
+
+    val backHandlingEnabled by remember { mutableStateOf(true) }
+    BackHandler(backHandlingEnabled) { }
 
     Box(
         modifier = modifier.fillMaxSize(),
