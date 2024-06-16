7b98c70d077c1b6df96fced173c5787ac7293d3c
hayashitoshiki <tosikihayasi@outlook.jp>
2024-06-16 15:20:29 +0900

    add param timeLimitRule in PlayerRule

--- domain/domain-object/src/main/java/com/example/domainObject/game/rule/PlayerRule.kt
+++ domain/domain-object/src/main/java/com/example/domainObject/game/rule/PlayerRule.kt
@@ -5,11 +5,13 @@ package com.example.domainObject.game.rule
  * 各手番のルールまとめ
  *
  * @property hande 駒落ち設定
+ * @property timeLimitRule 持ち時間設定
  * @property isFirstCheckEnd 王手将棋の適用
  * @property isTryRule トライルールの適用
  */
 data class PlayerRule(
     val hande: Hande = Hande.NON,
+    val timeLimitRule: TimeLimitRule = TimeLimitRule.None,
     val isFirstCheckEnd: Boolean = false,
     val isTryRule: Boolean = true,
 ) {
--- domain/test-domain-object/src/main/java/com/example/testDomainObject/rule/FakePlayerRule.kt
+++ domain/test-domain-object/src/main/java/com/example/testDomainObject/rule/FakePlayerRule.kt
@@ -2,16 +2,18 @@ package com.example.testDomainObject.rule
 
 import com.example.domainObject.game.rule.Hande
 import com.example.domainObject.game.rule.PlayerRule
+import com.example.domainObject.game.rule.TimeLimitRule
 
 fun PlayerRule.Companion.fake(
     hande: Hande = Hande.NON,
+    timeLimitRule: TimeLimitRule = TimeLimitRule.None,
     isFirstCheckEnd: Boolean = false,
     isTryRule: Boolean = false,
 ): PlayerRule {
     return PlayerRule(
         hande = hande,
+        timeLimitRule = timeLimitRule,
         isFirstCheckEnd = isFirstCheckEnd,
         isTryRule = isTryRule,
     )
-
 }
